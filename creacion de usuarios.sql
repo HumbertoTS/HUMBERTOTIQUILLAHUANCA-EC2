37e613dc0a1b98a3bc7b62e2df3e6cad2a807060
HumbertoTS <hronny.tsalvardor@gmail.com>
2022-10-26 01:18:20 -0500

    seguridad

--- creacion de usuarios.sql
+++ creacion de usuarios.sql
@@ -1,4 +1,4 @@
 use bodega;
 SELECT * FROM bodega.usuario;
 insert into usuario (usuario, password, rol ) values ("empleado","123456","USER");
-insert into usuario (usuario, password, rol ) values ("admin","123","ADMIN");
+insert into usuario (usuario, password, rol ) values ("admin","123456","ADMIN");
